a8c2c8b34bf2151390be7926af95cf27a307df6b
Alicia <vampgirl_18@yahoo.com>
2012-10-10 15:01:46 -0700

    Prime Library
    Made a prime library from the 2 prime functions that I used in problem
    3. I didn't this so that I could use them for problem 7.

---
+++ Prime/Prime/Prime.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+	public class Prime
+	{
+		public Prime(){ }
+
+		//Code taken and altered from http://www.dotnetperls.com/prime
+		public bool isPrime(long candidate)
+		{
+
+			if ((candidate & 1) == 0)
+			{
+				if (candidate == 2)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			for (long i = 3; (i * i) <= candidate; i += 2)
+			{
+				if ((candidate % i) == 0)
+				{
+					return false;
+				}
+			}
+			return candidate != 1;
+		}
+
+		public long findNextPrime(long fromHere)
+		{
+			long prime = 0;
+			bool primeFound = false;
+
+			if (isPrime(fromHere))
+			{
+				prime = fromHere;
+			}
+
+			else
+			{
+				while (!primeFound)
+				{
+					if (isPrime(++fromHere))
+					{
+						prime = fromHere;
+						primeFound = true;
+					}
+				}
+			}
+
+			return prime;
+		}
+	}
+}
