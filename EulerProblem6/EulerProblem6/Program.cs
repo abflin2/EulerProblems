e96657cca05eb6db0f590edab9b029f6d34c1267
Alicia <vampgirl_18@yahoo.com>
2012-10-10 09:35:43 -0700

    Problem 6 - Finished.
    That was a easy one...

--- EulerProblem6/EulerProblem6/Program.cs
+++ EulerProblem6/EulerProblem6/Program.cs
@@ -9,6 +9,44 @@ namespace EulerProblem6
 	{
 		static void Main(string[] args)
 		{
+			int targetNum = 100;
+			int difference = 0;
+			int sum = sumOfSquares(targetNum);
+			int square = squareOfSum(targetNum);
+			
+
+			if(sum > square) 
+				difference = sum - square;
+			else 
+				difference = square - sum;
+
+			Console.WriteLine("The difference between the sum of the squares and the square of the sums for " + targetNum + " is " + difference);
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
 		}
+
+		static int sumOfSquares(int num)
+		{
+			int total = 0;
+			for (int i = 1; i <= num; i++)
+			{
+				total += i * i;
+			}
+
+			return total;
+		}
+
+		static int squareOfSum(int num)
+		{
+			int total = 0;
+			for (int i = 1; i <= num; i++)
+			{
+				total += i;
+			}
+
+			total *= total;
+			return total;
+		}
+
 	}
 }
