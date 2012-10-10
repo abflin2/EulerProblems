cf34a9cf8ac5668643af1b51160319f968904607
Alicia <vampgirl_18@yahoo.com>
2012-10-10 09:08:49 -0700

    Problem 5 added
    Correctly answered. Checked on site.
    
    2520 is the smallest number that can be divided by each of the numbers
    from 1 to 10 without any remainder.
    
    What is the smallest positive number that is evenly divisible by all of
    the numbers from 1 to 20?

---
+++ EulerProblem5/EulerProblem5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace EulerProblem5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool divisible = false;
+			int top = 20;
+			int num = top -1; // I know that no number under top can be divisible by all numbers under top, so I'll start here.
+			DateTime start = DateTime.Now;
+			while (!divisible)
+			{
+				num++;
+				
+				for (int i = 2; i <= top; i+=2)
+				{
+					if (num % i != 0)
+					{
+						divisible = false;
+						break;
+					}
+
+					else
+						divisible = true;
+				}
+			}
+			TimeSpan time = DateTime.Now - start ;
+			Console.WriteLine("The smallest number divisible by all numbers from 1 to " + top + " is: " + num);
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+}
